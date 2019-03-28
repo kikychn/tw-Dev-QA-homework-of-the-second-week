beb247be09bf2318c283a4ae7f6d7933bd3ce01d
kikychn <kikychn@gmail.com>
2019-03-28 21:30:54 +0800

    update PracticeB in section1

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,18 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List<String> result = new ArrayList<>();
+        for (List<String> strs : collection2) {
+            for (String s : collection1) {
+                if (strs.contains(s)) {
+                    result.add(s);
+                }
+            }
+        }
+        return result;
     }
 }
