4b766ffdd6be136474627e6c5a35c1ff43ab060f
kikychn <kikychn@gmail.com>
2019-03-29 15:19:59 +0800

    update PracticeC in section1

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,20 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
-    List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+    List<String> collectSameElements(List<String> collection1, Map<String, List<String>> collection2) {
+        List<String> result = new ArrayList<>();
+        for (String s : collection1) {
+            for (String s1 : collection2.get("value")) {
+                if (s == s1) {
+                    result.add(s);
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
