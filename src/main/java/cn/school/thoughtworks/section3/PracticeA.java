c481723cb2331c057493e428ee23ffbf384d6168
kikychn <kikychn@gmail.com>
2019-03-29 19:18:47 +0800

    update PracticeA in section3

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,12 +1,19 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
-    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
+    Map<String, Integer> createUpdatedCollection(Map<String, Integer> collectionA, Map<String, List<String>> object) {
+        Map<String, Integer> result = collectionA;
+        for (String key : object.get("value")) {
+            Integer count = collectionA.get(key);
+            if (count != null) {
+                result.put(key, count - 1);
+            }
+        }
 
-        return null;
+        return result;
     }
 }
