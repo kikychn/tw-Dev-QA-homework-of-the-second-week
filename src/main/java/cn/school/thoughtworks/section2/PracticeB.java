fed19ec5c23dd9437ab80f91ea17168f7e4e74ab
kikychn <kikychn@gmail.com>
2019-03-29 18:05:01 +0800

    update PracticeB in section2

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -8,9 +8,25 @@ public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
         Map<String, Integer> result = new HashMap<String, Integer>();
         for (String key : collection1) {
-            if (key.contains("-")) {
-                String[] list = key.split("-");
-                result.put(list[0], Integer.parseInt(list[1]));
+            String specialChar = "-";
+            if (key.contains(specialChar)) {
+                char[] list = key.toCharArray();
+                String mapKey = String.valueOf(list[0]);
+                String mapCount = "";
+                for (int i = 2; i < list.length; i++) {
+//                    try {
+//                        Integer tempCount = Integer.parseInt(String.valueOf(list[i]));
+//                        if (tempCount >= 0 && tempCount <= 9) {
+//                            mapCount += list[i];
+//                        }
+//                    } catch (NumberFormatException e) {
+//                        break;
+//                    }
+                    if (list[i]>='0' && list[i]<='9') {
+                        mapCount += list[i];
+                    }
+                }
+                result.put(mapKey, Integer.valueOf(mapCount));
             } else {
                 Integer count = result.get(key);
                 result.put(key, (count == null) ? 1 : count + 1);
